41f29b02e83e11e73b567ef8b299be3d9fbaeed4
Eric Huffman <eric@erichuffman.com>
2015-05-04 20:58:04 -0400

    adding bundler support, breakpoint library, and doing some general cleanup

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -12,7 +12,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><?php print $head; ?>
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="MobileOptimized" content="width">
   <meta name="HandheldFriendly" content="true">
   <meta http-equiv="cleartype" content="on">
@@ -20,13 +20,11 @@
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <!--[if lte IE 8]><div class="browser-warning lte-ie8"><![endif]-->
   <p id="skip-link">
     <a href="#content" class="element-invisible element-focusable">Skip To Content</a>
   </p>
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
-  <!--[if lte IE 8]></div><![endif]-->
 </body>
 </html>
